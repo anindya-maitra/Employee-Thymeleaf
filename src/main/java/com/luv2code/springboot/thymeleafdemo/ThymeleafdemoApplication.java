9386e436f29a50c82878f19b17b24ce205a3ff3a
anindya-maitra <anindyamaitra007@gmail.com>
2024-02-06 11:12:04 +0530

    final commit

--- src/main/java/com/luv2code/springboot/thymeleafdemo/ThymeleafdemoApplication.java
+++ src/main/java/com/luv2code/springboot/thymeleafdemo/ThymeleafdemoApplication.java
@@ -5,7 +5,6 @@ import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.boot.builder.SpringApplicationBuilder;
 import org.springframework.boot.web.servlet.support.SpringBootServletInitializer;
 
-import com.example.demo.JenkinsDemoApplication;
 
 @SpringBootApplication
 public class ThymeleafdemoApplication extends SpringBootServletInitializer{
